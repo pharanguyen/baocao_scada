50de309767ea3bd1ed0fe9db943415643fced166
huybui21 <huy002001@gmail.com>
2023-09-29 10:06:56 +0700

    Add:Dm_ChiNhanh

---
+++ DAO/Services/DanhMuc/DmChiNhanhService.cs
@@ -0,0 +1,80 @@
+﻿using DAO.Models.CommonModels;
+using DAO.Models.DanhMuc;
+using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAO.Services.DanhMuc
+{
+    public class DmChiNhanhService
+    {
+        public static ResultModel<List<Dm_ChiNhanh>> GetToComBo()
+        {
+            try
+            {
+                var _db = new SqlHelper();
+                return new ResultModel<List<Dm_ChiNhanh>>() { Data = _db.GetAll<Dm_ChiNhanh>().ToList() };
+            }
+            catch (Exception ex) { return new ResultModel<List<Dm_ChiNhanh>>() { isThanhCong = false, ThongBao = ex.Message }; }
+        }
+
+        public static ResultModel<List<Dm_ChiNhanh>> GetAll()
+        {
+            try
+            {
+                var _db = new SqlHelper();
+                return new ResultModel<List<Dm_ChiNhanh>>() { Data = _db.GetAll<Dm_ChiNhanh>().ToList() };
+            }
+            catch (Exception ex) { return new ResultModel<List<Dm_ChiNhanh>>() { isThanhCong = false, ThongBao = ex.Message }; }
+        }
+
+        public static ResultModel<Dm_ChiNhanh> GetById(int ms_bd)
+        {
+            try
+            {
+                var _db = new SqlHelper();
+                var _obj = _db.GetSingleEntityById<Dm_ChiNhanh>(ms_bd);
+                if (_obj == null) throw new Exception(_db.LoiNgoaiLe);
+                return new ResultModel<Dm_ChiNhanh>() { Data = _obj };
+            }
+            catch (Exception ex) { return new ResultModel<Dm_ChiNhanh>() { isThanhCong = false, ThongBao = ex.Message }; }
+        }
+        public static ResultModel<int?> Add(Dm_ChiNhanh entity)
+        {
+            try
+            {
+                var _db = new SqlHelper();
+                var objID = _db.InsertEntity(entity);
+                entity.Id = (int)objID;
+                if (objID == null) throw new Exception(_db.LoiNgoaiLe);
+                return new ResultModel<int?>() { Data = 1 };
+            }
+            catch (Exception ex)
+            { return new ResultModel<int?>() { isThanhCong = false, ThongBao = ex.Message }; }
+        }
+
+        public static ResultModel<int?> Update(Dm_ChiNhanh entity)
+        {
+            try
+            {
+                var _db = new SqlHelper();
+                var objId = _db.UpdateEntity(entity);
+                if (objId == null) { throw new Exception(_db.LoiNgoaiLe); }
+                return new ResultModel<int?>() { Data = _db.UpdateEntity(entity) };
+            }
+            catch (Exception ex) { return new ResultModel<int?>() { isThanhCong = false, ThongBao = ex.Message }; }
+        }
+        public static ResultModel<int?> DeleteById(int id)
+        {
+            try
+            {
+                var _db = new SqlHelper();
+                return new ResultModel<int?>() { Data = _db.DeleteEntityById<Dm_ChiNhanh>(id) };
+            }
+            catch (Exception ex) { return new ResultModel<int?>() { isThanhCong = false, ThongBao = ex.Message }; }
+        }
+    }
+}
---
+++ ERPProject/Pages/DanhMuc/Dm_ChiNhanh_CapNhat.razor.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using DAO.Models.CommonModels;
+using DAO.Models.DanhMuc;
+using DAO.Services.DanhMuc;
+using ERPProject.Services;
+using Microsoft.AspNetCore.Components;
+
+namespace ERPProject.Pages.DanhMuc
+{
+    public partial class Dm_ChiNhanh_CapNhatBase : ComponentBase
+    {
+        public bool isShow = false;
+        public bool isThemMoi = false;
+        public string TieuDe = "";
+        public bool isDaCapNhatDuLieu = false;
+        [Parameter]
+        public Action<bool> OnLuu { get; set; }
+        public int objID { get; set; }
+        //khai báo hiển thị thông báo
+        [Inject]
+        protected ToastService toastService { get; set; }
+
+        public DAO.Models.DanhMuc.Dm_ChiNhanh objModel = new DAO.Models.DanhMuc.Dm_ChiNhanh();
+        public void Show(int _id)
+        {
+            objID = _id;
+            if (!isThemMoi && objID != 0)
+            {
+                var rsModel = DmChiNhanhService.GetById(_id);
+                if (rsModel.isThanhCong) objModel = rsModel.Data;
+                else
+                {
+                    toastService.ShowDanger(rsModel.ThongBao);
+                    return;
+                }
+            }
+            else
+            {
+                objModel = new DAO.Models.DanhMuc.Dm_ChiNhanh();
+            }
+            isShow = true;
+            StateHasChanged();
+        }
+        protected async void onSave()
+        {
+            if (objModel.Ten == null)
+            {
+                toastService.ShowWarning("Chưa nhập tên chi nhánh !");
+                // tbHoTen.FocusIn();
+                return;
+            }
+            try
+            {
+                if (objID == 0)
+                {
+                    var rsModel = new ResultModel<int?>();
+                    await Task.Run(() => { rsModel = DmChiNhanhService.Add(objModel); });
+                    if (!rsModel.isThanhCong) throw new Exception(rsModel.ThongBao);
+                    toastService.ShowSuccess("Đã thêm chi nhánh thành công!");
+                    isShow = false;
+                }
+                else
+                {
+                    var rsModel = new ResultModel<int?>();
+                    await Task.Run(() => { rsModel = DmChiNhanhService.Update(objModel); });
+                    if (!rsModel.isThanhCong) throw new Exception(rsModel.ThongBao);
+                    toastService.ShowSuccess("Đã cập nhật!");
+                    isDaCapNhatDuLieu = true;
+                    isShow = false;
+                }
+            }
+            catch (Exception ex)
+            {
+                toastService.ShowDanger(ex.Message);
+            }
+            StateHasChanged();
+        }
+
+        public void onClose()
+        {
+            isShow = false;
+            StateHasChanged();
+        }
+
+
+    }
+}

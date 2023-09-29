e5c8127cf931fa2a51e993e97f18f51230076d46
huybui21 <huy002001@gmail.com>
2023-09-29 10:06:56 +0700

    Add:Dm_ChiNhanh

---
+++ DAO/Models/DanhMuc/Dm_ChiNhanh.cs
@@ -0,0 +1,19 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAO.Models.DanhMuc
+{
+    public class Dm_ChiNhanh
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Ten { get; set; }
+        public string Ma { get; set; }
+        public string Dia_Chi { get; set; }
+        public int Stt { get; set; }
+    }
+}
---
+++ ERPProject/Pages/DanhMuc/Dm_ChiNhanh.razor.cs
@@ -0,0 +1,143 @@
+﻿using DAO.Models.CommonModels;
+using DAO.Models.DanhMuc;
+using DAO.Models.PhanQuyen;
+using DAO.Services.DanhMuc;
+using DAO.Services.PhanQuyen;
+using ERPProject.Services;
+using ERPProject.Shared;
+using Microsoft.AspNetCore.Components;
+using Syncfusion.Blazor.Grids;
+
+
+namespace ERPProject.Pages.DanhMuc
+{
+    public class DmChiNhanhBase : ComponentBase
+    {
+        public List<DAO.Models.DanhMuc.Dm_ChiNhanh> listBienDoc { get; set; }
+        protected Dm_ChiNhanh_CapNhat fCapNhat;
+        ds_phanquyen _QSD = new ds_phanquyen();
+        [Inject]
+        AppDataScoped AppData { get; set; }
+        protected FormXacNhan frmXacNhan;
+        [Inject]
+        protected ToastService toastService { get; set; }
+        [Inject]
+        protected Blazored.LocalStorage.ILocalStorageService localStorage { get; set; }
+        protected SfGrid<DAO.Models.DanhMuc.Dm_ChiNhanh> gdv;
+        protected override void OnInitialized()
+        {
+            Task.Run(() =>
+            {
+                InvokeAsync(async () =>
+                {
+                    var _tokenE = await localStorage.GetItemAsync<string>("Token");
+                    var _token = MsUtils.Encryption.DecryptString(_tokenE);
+                    var strInfo = _token.Split("@MS@");
+                    if (strInfo.Length == 5)
+                    {
+                        var rsModel = new ResultModel<ds_phanquyen>();
+                        await Task.Run(() => { rsModel = ds_phanquyenService.GetQuyenThanhVien(Convert.ToInt32(strInfo[0]), "btnChiNhanhCN"); });
+                        _QSD = rsModel.Data;
+
+
+                    }
+                    onTaiLai();
+
+
+                });
+            });
+            base.OnInitialized();
+        }
+
+        protected async void onTaiLai()
+        {
+
+            AppData.loadingPanel.show();
+            var rsModel = new ResultModel<List<DAO.Models.DanhMuc.Dm_ChiNhanh>>();
+            await Task.Run(() => { rsModel = DmChiNhanhService.GetAll(); });
+            if (rsModel.isThanhCong)
+            {
+                listBienDoc = rsModel.Data;
+                AppData.loadingPanel.hide();
+            }
+            else
+            {
+                AppData.loadingPanel.hide();
+                toastService.ShowDanger(rsModel.ThongBao);
+                return;
+            }
+            StateHasChanged();
+        }
+
+        protected void onThemMoi()
+        {
+            //if (_QSD.them == false)
+            //{
+            //    toastService.ShowWarning("Bạn không có quyền sử dụng tính năng này !");
+            //    return;
+            //}
+            fCapNhat.isThemMoi = true;
+            fCapNhat.TieuDe = "Thêm";
+            fCapNhat.Show(0);
+            onTaiLai();
+        }
+        protected void onCapNhat(int _ID)
+        {
+            //if (_QSD.sua == false)
+            //{
+            //    toastService.ShowWarning("Bạn không có quyền sử dụng tính năng này !");
+            //    return;
+            //}
+            fCapNhat.TieuDe = "Cập nhật thông tin";
+            fCapNhat.isThemMoi = false;
+            fCapNhat.Show(_ID);
+            onTaiLai();
+        }
+
+        protected void XacNhanLuu(bool isLuuThanhCong)
+        {
+            if (isLuuThanhCong)
+            {
+                onTaiLai();
+            }
+
+            // StateHasChanged();
+            //   gdv.Refresh();
+        }
+
+        protected void onXoa(int _ID)
+        {
+            //check quyen xoa
+            //if (_QSD.xoa == false || _QSD.xoa == null)
+            //{
+            //    toastService.ShowWarning("Bạn không có quyền sử dụng tính năng này !");
+            //    return;
+            //}
+            frmXacNhan.Show("Xóa dòng được chọn ?", "300px", new System.Action(async () =>
+            {
+                AppData.loadingPanel.show();
+                var rsModel = new ResultModel<int?>();
+                await Task.Run(() => { rsModel = DmChiNhanhService.DeleteById(_ID); });
+                AppData.loadingPanel.hide();
+                if (rsModel.isThanhCong) onTaiLai();
+                else toastService.ShowDanger(rsModel.ThongBao);
+            }));
+        }
+        public async Task onXuatExcel()
+        {
+
+            ExcelExportProperties excelExportProperties = new ExcelExportProperties();
+            excelExportProperties.FileName = "Danh sach bien doc.xlsx";
+            var selectedRecord = await gdv.GetSelectedRecordsAsync();
+            if (selectedRecord.Count() > 0)
+            {
+                excelExportProperties.DataSource = selectedRecord;
+            }
+            else
+            {
+                excelExportProperties.DataSource = listBienDoc;
+            }
+            await gdv.ExportToExcelAsync(excelExportProperties);
+        }
+    }
+}

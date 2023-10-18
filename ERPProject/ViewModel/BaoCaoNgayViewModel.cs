43dec30fd20bd31339f64fc246e309ba9b5204ac
thetp <thehp9@gmail.com>
2023-10-18 11:01:06 +0700

    update thay đổi cấu trúc table -> SfGrid

---
+++ ERPProject/ViewModel/BaoCaoNgayViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace ERPProject.ViewModel
+{
+    public class BaoCaoNgayViewModel
+    {
+        public string? ThoiGian { get; set; }
+        public List<ThongSoTram>? dataTram { get; set; }
+    }
+    public class ThongSoTram
+    {
+        public string? TenTram { get; set; }
+        public string? LuuLuongTucThoi { get; set; }
+        public string? TongLuuLuong { get; set; }
+    }
+}

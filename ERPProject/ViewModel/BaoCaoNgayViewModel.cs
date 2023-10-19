fc158033c079bb3ff79f40f99dbec246c7b0cf5c
thetp <thehp9@gmail.com>
2023-10-19 15:28:27 +0700

    update tối ưu load báo cáo ngày

--- ERPProject/ViewModel/BaoCaoNgayViewModel.cs
+++ ERPProject/ViewModel/BaoCaoNgayViewModel.cs
@@ -2,13 +2,6 @@
 {
     public class BaoCaoNgayViewModel
     {
-        public string? ThoiGian { get; set; }
-        public List<ThongSoTram>? dataTram { get; set; }
-    }
-    public class ThongSoTram
-    {
-        public string? TenTram { get; set; }
-        public string? LuuLuongTucThoi { get; set; }
-        public string? TongLuuLuong { get; set; }
+        public DateTime ThoiGian { get; set; }
     }
 }
